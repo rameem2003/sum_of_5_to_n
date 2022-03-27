d1d5923eab0b99416e3579d61c785f1b720fb8e4
rameem2003 <rameem2019@gmail.com>
2022-03-27 21:40:46 +0600

    Add project files.

---
+++ sum_of_5_to_n/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace app
+{
+    class app
+    {
+        public static void Main()
+        {
+            int a = 5;
+            int sum = 0;
+
+            for(a = 5; a<= 100; a = a + 5)
+            {
+                sum = sum + a;
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
